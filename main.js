f360e6067c61219782c679d327f09ec739bf6645
Dominic Serrano <dmsrojo@gmail.com>
2017-04-03 20:26:32 -0500

    Added example using a read/write computed observable.

--- main.js
+++ main.js
@@ -14,9 +14,32 @@ const genViewModel = ({first, last}) => {
   return {
     firstName: kob(first),
     lastName: kob(last),
-    fullName: (first, last) => `${first()} ${last()}`
+    // fullName: (first, last) => `${first()} ${last()}`
   }
 };
 
+
+// Creating a new view model object
+const myComputed = genViewModel(data);
+
+
+// adding a read/write computed value to the view model object
+myComputed.fullName = ko.computed({
+  // This will concat firstName and lastName
+  read: function() {
+    return `${this.firstName()} ${this.lastName()}`;
+  },
+  // This will change the firstName and lastName when fullName is edited
+  write: function(string) {
+    let first = string.split(" ")[0] ? string.split(" ")[0] : "";
+    let last = string.split(" ")[1] ? string.split(" ")[1] : "";
+    this.firstName(first);
+    this.lastName(last);
+  },
+  // Passing in myComputed for reference to this
+  owner: myComputed
+});
+
+
 // Apply view model to the view
-ko.applyBindings(genViewModel(data));
+ko.applyBindings(myComputed);
