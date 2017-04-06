75188ccedeb923ba74a6d5377f56a04efd5324d6
Dominic Serrano <dmsrojo@gmail.com>
2017-04-05 20:07:28 -0500

    Semi working observable subscription

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,12 @@ require(['objectObs', 'arrayObs', 'subs'],
 
   (objects, array, subs) => {
 
+  // Use promise.all to load all vm's
   Promise.all([
     objects, array, subs()
   ])
   .then(([object, array, subs]) => (
-    // Apply view model to the view
+    // Apply view models to the view
     ko.applyBindings({
       objects,
       array,
--- scripts/subs.js
+++ scripts/subs.js
@@ -3,15 +3,25 @@
 define(['helper'], ({kob}) => {
 
   const loadCarVM = ({makes, models, cars}) => ({
-    make: kob(makes),
-    model: kob(models),
-    car: kob(cars)
+    make: ko.observableArray(makes),
+    model: ko.observableArray(models),
+    car: ko.observableArray(cars),
+    selectedMake: kob(),
+    selectedModel: kob(),
+    selectedCar: kob()
   });
+
+  const setSubscriptions = (vm) => {
+    vm.selectedMake.subscribe(() => vm.selectedModel(undefined));
+    vm.selectedModel.subscribe(() => vm.selectedCar(undefined));
+    return vm;
+  };
 
   const getSubs = () => {
     return fetch('./data/subs.json')
     .then(data => data.json())
     .then(loadCarVM)
+    .then(setSubscriptions)
     .catch(console.log)
   };
 
